be28f1e08042f763bb8f77ed6627b1026a9d57dd
lihuiping <515058827@qq.com>
2017-05-22 03:15:58 +0000

    no commit message

--- src/assets/js/movie_index.js
+++ src/assets/js/movie_index.js
@@ -1,46 +1,43 @@
-
-var conf = 1;
-var bann = ['../../assets/data/index.json', 'http://v.zy7c.com/index.php?r=lunbo']; //banner
-var hotdata = ['./assets/data/goods.json', baseUrl()+'index.php/Api/Movie/lists?type=1']; //热映大片
-var yuxian_ry=['./assets/data/goods.json', baseUrl()+'index.php/Api/Movie/lists?type=2']; //院线热映
-var wl_movie=['./assets/data/goods.json', baseUrl()+'index.php/Api/Movie/lists?type=3'];  //网络电影
-var jd_movie=['./assets/data/goods.json', baseUrl()+'index.php/Api/Movie/lists?type=4'];  //经典影院
-var zzdx_movie=['./assets/data/goods.json', baseUrl()+'index.php/Api/Zhizun/get_one'];  //至尊独享
-
+var conf = 0;
+var banner = ['./assets/data/banner.json', baseUrl() + 'index.php/Api/Carousel/index']; //banner
+var hotdata = ['./assets/data/list.json', baseUrl() + 'index.php/Api/Movie/lists?type=1']; //热映大片
+var yuxian_ry = ['./assets/data/list.json', baseUrl() + 'index.php/Api/Movie/lists?type=2']; //院线热映
+var wl_movie = ['./assets/data/list.json', baseUrl() + 'index.php/Api/Movie/lists?type=3']; //网络电影
+var jd_movie = ['./assets/data/list.json', baseUrl() + 'index.php/Api/Movie/lists?type=4']; //经典影院
+var zzdx_movie = ['./assets/data/zzdx.json', baseUrl() + 'index.php/Api/Zhizun/get_one']; //至尊独享
 /*懒加载*/
 function imgload() {
-				$("img.lazy").scrollLoading({
-					attr: "data-src",
-					callback: function() {
-						var $this = $(this);
-						$this.removeClass("lazy").removeAttr("data-src");
-						var src = $this.attr("src");
-						if(src.toLowerCase().indexOf("uploadfile") >= 0 || src.toLowerCase().indexOf("myupload") >= 0 || src.toLowerCase().indexOf("http") == -1) {}
-					}
-				});
-			}
-			window.setTimeout(function() { imgload(); }, 0);
-			var timer = null;
-			$(".content").on("scroll", function() {
-				if(timer) { clearTimeout(timer); }
-				timer = setTimeout(function() { imgload(); }, 0);
-			});
-			$(".content").trigger("scroll");
-			
+	$("img.lazy").scrollLoading({
+		attr: "data-src",
+		callback: function() {
+			var $this = $(this);
+			$this.removeClass("lazy").removeAttr("data-src");
+			var src = $this.attr("src");
+			if(src.toLowerCase().indexOf("uploadfile") >= 0 || src.toLowerCase().indexOf("myupload") >= 0 || src.toLowerCase().indexOf("http") == -1) {}
+		}
+	});
+}
+window.setTimeout(function() { imgload(); }, 0);
+var timer = null;
+$(".content").on("scroll", function() {
+	if(timer) { clearTimeout(timer); }
+	timer = setTimeout(function() { imgload(); }, 0);
+});
+$(".content").trigger("scroll");
+
 // banner图
 $.ajax({       
 	type: "GET",
 	       async: false,
-	       url: 'http://www.movie.com/index.php/Api/Carousel/index', //实际上访问时产生的地址为: http://www.my.com/index.php?r=lunbo
-	       cache: false, //默认值true
+	       url: banner[conf], //实际上访问时产生的地址为: http://www.my.com/index.php?r=lunbo
+	       //cache: false, //默认值true
 	       dataType : 'json',
-
 	       success: function(json) {  
 		var dataList = json;
 		var app = new Vue({
 			el: '#index-banner',
 			data: dataList,
-		}); 
+		});
 		var mySwiper = new Swiper('.swiper-container', {
 			autoplay: 1000, //可选选项，自动滑动
 			pagination: '.swiper-pagination',
@@ -54,7 +51,7 @@ var goods = new Vue({
 	el: "#hot-list",
 	data: {
 		goodslist: [],
-		loadingFlag:true,
+		loadingFlag: true,
 	},
 	mounted: function() {
 		//初始化加载数据
@@ -84,7 +81,7 @@ var yx_ry = new Vue({
 	el: "#yx-list",
 	data: {
 		goodslist: [],
-		loadingFlag:true,
+		loadingFlag: true,
 	},
 	mounted: function() {
 		//初始化加载数据
@@ -114,7 +111,7 @@ var wl_movie = new Vue({
 	el: "#wl_movie",
 	data: {
 		goodslist: [],
-		loadingFlag:true,
+		loadingFlag: true,
 	},
 	mounted: function() {
 		//初始化加载数据
@@ -144,7 +141,7 @@ var jd_movie = new Vue({
 	el: "#jd_movie",
 	data: {
 		goodslist: [],
-		loadingFlag:true,
+		loadingFlag: true,
 	},
 	mounted: function() {
 		//初始化加载数据
@@ -168,13 +165,13 @@ var jd_movie = new Vue({
 			return 'goods-detail.html?id=' + val
 		},
 	}
-});/*至尊独享*/
+}); /*至尊独享*/
 /*首页至尊独享*/
 var zzdx_movie = new Vue({
 	el: "#zzdx_movie",
 	data: {
 		goodslist: [],
-		loadingFlag:true,
+		loadingFlag: true,
 	},
 	mounted: function() {
 		//初始化加载数据
