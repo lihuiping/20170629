8493ac20271ac18254cf71ffbbd5666781f7fda9
lihuiping <515058827@qq.com>
2017-05-24 09:31:23 +0000

    no commit message

---
+++ src/assets/js/user-center.js
@@ -0,0 +1,55 @@
+$(function(){
+//普通会员个人中心渲染
+	//编辑资料跳转	
+	$("#p-message").on("click",function(){
+			window.location.href="edit-message.html"
+	})
+	//会员特权
+	$("#huiyuan-page").on("click",function(){
+			window.location.href="/templets/gerenzhongxin/huiyuan/khuiyuan.html"
+	})
+	
+	//推荐页跳转
+	$("#tuijian-page").on("click",function(){
+			window.location.href="tuijianYouli.html"
+	})
+	
+	//卡卷页
+	$("#kajuan-page").on("click",function(){
+			window.location.href="/templets/gerenzhongxin/wodekajuan/wodekajuan.html"
+	})
+	
+	//播放记录
+	$("#bfjl-page").on("click",function(){
+			window.location.href="/templets/gerenzhongxin/bofangJilu/bfjl.html"
+	})
+	//信息中心
+	$("#xxzx-page").on("click",function(){
+			window.location.href="/templets/gerenzhongxin/xiaoxizhongxin/xiaoxizhongxin.html"
+	})
+	//我的电影票
+	$("#wddyp-page").on("click",function(){
+			window.location.href="/templets/gerenzhongxin/wodedianyingpiao/wddianyingpiao.html"
+	})
+	//意见反馈
+	$("#yjfk-page").on("click",function(){
+			window.location.href="/templets/gerenzhongxin/yijianfankui/yijianfankui.html"
+	})
+	//设置
+	$("#shezhi-page").on("click",function(){
+			window.location.href="my-setting.html"
+	})
+	//视频收藏
+	$("#spsc-page").on("click",function(){
+			window.location.href="/templets/gerenzhongxin/shipinshoucang/spsc.html"
+	})
+	//我的积分
+	$("#wdjf-page").on("click",function(){
+			window.location.href="/templets/gerenzhongxin/wodejifen/MyPoints.html"
+	});
+	//我的订单
+	$("#dingdan-page").on("click",function(){
+			window.location.href="/templets/gerenzhongxin/wodedingdan/wodedingdan.html"
+	});
+
+});
