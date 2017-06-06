2f038a0e892866b38ba1b0bd5686f8c0842b17e9
lihuiping <515058827@qq.com@abe6062b-bf53-4d67-bccd-e0020afaa145>
2017-06-06 02:10:13 +0000

    no commit message

--- src/assets/js/movie_index.js
+++ src/assets/js/movie_index.js
@@ -1,9 +1,9 @@
-var conf = 0;
+var conf = 1;
 var banner = ['./assets/data/banner.json', baseUrl() + 'index.php/Api/Carousel/index']; //banner
-var hotdata = ['./assets/data/list.json', baseUrl() + 'index.php/Api/Movie/lists?type=1']; //热映大片
-var yuxian_ry = ['./assets/data/list.json', baseUrl() + 'index.php/Api/Movie/lists?type=2']; //院线热映
-var wl_movie = ['./assets/data/list.json', baseUrl() + 'index.php/Api/Movie/lists?type=3']; //网络电影
-var jd_movie = ['./assets/data/list.json', baseUrl() + 'index.php/Api/Movie/lists?type=4']; //经典影院
+var hotdata = ['./assets/data/list.json', baseUrl() + 'tv/?s=/Api/movie/lists/type/1']; //热映大片
+var yuxian_ry = ['./assets/data/list.json', baseUrl() + 'tv/?s=/Api/movie/lists/type/2']; //院线热映
+var wl_movie = ['./assets/data/list.json', baseUrl() + 'tv/?s=/Api/movie/lists/type/3']; //网络电影
+var jd_movie = ['./assets/data/list.json', baseUrl() + 'tv/?s=/Api/movie/lists/type/4']; //经典影院
 var zzdx_movie = ['./assets/data/zzdx.json', baseUrl() + 'index.php/Api/Zhizun/get_one']; //至尊独享
 /*懒加载*/
 function imgload() {
@@ -73,7 +73,7 @@ var goods = new Vue({
 		},
 		getTitleHref: function(val) {
 //  		return 'goods-detail.html?id=' + val
-			return 'movie-content.html'
+			return 'movie-content.html?id='+val
 		},
 	}
 });
