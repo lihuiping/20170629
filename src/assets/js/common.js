035e033ce6dc5f7ee26c1ec62227bb3e5373ad7b
汪盛龙 <shenglongok@163.com>
2017-06-23 06:51:01 +0000

    no commit message

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -143,17 +143,17 @@ $(function() {
 		Cache.set('token', tokEn);
 	};
 
-	$(".bar-tab").on('click', ".tab-item", function(event) {
-		var th = $(this);
-		var ss = $(this).siblings('.tab-item');
-		var dtClass = th.data('class');
-		$(this).find(".icon").prop('className', 'icon ' + dtClass + '-active');
-		$.each(ss, function(index, val) {
-			var pard = $(this).data('class');
-			$(this).find('.icon').removeClass().addClass('icon ' + pard);
-
-		});
-	});
+//	$(".bar-tab").on('click', ".tab-item", function(event) {
+//		var th = $(this);
+//		var ss = $(this).siblings('.tab-item');
+//		var dtClass = th.data('class');
+//		$(this).find(".icon").prop('className', 'icon ' + dtClass + '-active');
+//		$.each(ss, function(index, val) {
+//			var pard = $(this).data('class');
+//			$(this).find('.icon').removeClass().addClass('icon ' + pard);
+//
+//		});
+//	});
 
 });
 //$(".bar-tab").on('click', ".tab-item", function(event) {
@@ -181,6 +181,7 @@ apiready();
 function openNewPage(name,uri){
 	api.openWin({
 		name:name,
-		url: uri
+		url: uri,
+		animation:{type:"none"}
 	});
 }
