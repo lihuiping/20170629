5435b615907a743522b22f7b1ff7c3b284ddbd32
Eric <7mus1c@163.com@abe6062b-bf53-4d67-bccd-e0020afaa145>
2017-06-26 08:26:15 +0000

    修改双击退出app

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -183,3 +183,28 @@ function openNewPage(name,uri){
 		animation:{type:"none"}
 	});
 }
+function exitApp(){
+        api.addEventListener({
+            name: 'keyback'
+        }, function(ret, err){
+            api.toast({
+                    msg: '再按一次返回键退出'+api.appName,
+                    duration:2000,
+                    location: 'bottom'
+                });
+                
+                api.addEventListener({
+                    name: 'keyback'
+                }, function(ret, err){
+                    api.closeWidget({
+                            id: 'A6960480793365',     //这里改成自己的应用ID
+                            retData: {name:'closeWidget'},
+                            silent:true
+                        });
+                });
+                
+                setTimeout(function(){
+                        exitApp();
+                },3000)
+        });
+}
