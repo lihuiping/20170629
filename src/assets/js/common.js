91fd22f8a1cd976381fb0c9d92a65dd867d327f2
Eric <7mus1c@163.com>
2017-06-26 07:32:46 +0000

    增加双击退出APP

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -174,6 +174,7 @@ var apiready = function() {
 	$api.fixStatusBar($api.dom('.bar-nav'));
 	$api.fixStatusBar($api.dom('.ad-bar'));
 	var offset = $api.offset('.ad-bar');
+	exitApp();
 };
 apiready();
 
@@ -184,4 +185,30 @@ function openNewPage(name,uri){
 		url: uri,
 		animation:{type:"none"}
 	});
+}
+
+function exitApp(){
+        api.addEventListener({
+            name: 'keyback'
+        }, function(ret, err){
+            api.toast({
+                    msg: '再按一次返回键退出'+api.appName,
+                    duration:2000,
+                    location: 'bottom'
+                });
+                
+                api.addEventListener({
+                    name: 'keyback'
+                }, function(ret, err){
+                    api.closeWidget({
+                            id: 'A6960480793365',     //这里改成自己的应用ID
+                            retData: {name:'closeWidget'},
+                            silent:true
+                        });
+                });
+                
+                setTimeout(function(){
+                        exitApp();
+                },3000)
+        });
 }
