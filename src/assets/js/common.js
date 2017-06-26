e81c99b12ba15728d8699b0d3ed34a64881c3446
Eric <7mus1c@163.com@abe6062b-bf53-4d67-bccd-e0020afaa145>
2017-06-26 11:43:17 +0000

    删除退出app

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -186,30 +186,3 @@ function openNewPage(name, uri) {
 	});
 }
 
-function exitApp() {
-	api.addEventListener({
-		name: 'keyback'
-	}, function(ret, err) {
-		api.toast({
-			msg: '再按一次返回键退出' + api.appName,
-			duration: 2000,
-			location: 'bottom'
-		});
-
-		api.addEventListener({
-			name: 'keyback'
-		}, function(ret, err) {
-			api.closeWidget({
-				id: 'A6940555676996', //这里改成自己的应用ID
-				retData: {
-					name: 'closeWidget'
-				},
-				silent: true
-			});
-		});
-
-		setTimeout(function() {
-			exitApp();
-		}, 3000)
-	});
-}
